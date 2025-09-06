965648c044d1848dfe0c72d06aec1ea6cb131af8
Mustafa Can Bildiren <78155451+mcanbildiren@users.noreply.github.com>
2025-09-07 01:01:38 +0300

    Ürün silme ve güncelleme sayfaları eklendi. Ürün listeleme sayfasına filtreler eklendi.

--- MVC_App_1/Controllers/ProductsController.cs
+++ MVC_App_1/Controllers/ProductsController.cs
@@ -6,12 +6,31 @@ namespace MVC_App_1.Controllers
 {
     public class ProductsController(ApplicationDbContext context) : Controller
     {
-
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(string name, decimal? minPrice, decimal? maxPrice)
         {
-            var products = context.Products.ToList();
-            return View(products);
+            var query = context.Products.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                query = query.Where(product => product.Name.Contains(name));
+            }
+
+            if (minPrice.HasValue)
+            {
+                query = query.Where(product => product.Price >= minPrice.Value);
+            }
+
+            if (maxPrice.HasValue)
+            {
+                query = query.Where(product => product.Price <= maxPrice.Value);
+            }
+
+            var products = query.ToList();
+
+            return View((products, name, minPrice, maxPrice));
         }
+
 
         [HttpGet]
         public IActionResult Create()
@@ -33,5 +52,54 @@ namespace MVC_App_1.Controllers
             var product = context.Products.Find(id);
             return View(product);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            context.Products.Update(product);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //[HttpGet]
+        //public IActionResult Delete(int id)
+        //{
+        //	var product = context.Products.Find(id);
+        //	return View(product);
+        //}
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if (product != null)
+            {
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
+
+//1. Yöntem: Bütün kayıtları çekip, sonrasında filtreleme yapmak. Gereksiz kayıtlar da gelir, performans çok düşük.
+//var products = context.Products.ToList();
+//products.Where(x=>x.Name == name);
+
+//2. Yöntem: Daha okunaklı ama daha fazla kod yazılan yöntem. 3. yöntemle arasında performans farkı yok.
+//var products = context.Products.AsQueryable();
+//products.Where(x=>x.Name == name);
+//products.OrderBy(x => x.Name);
+//products.ToList();
+
+//3. Yöntem: Tek satırda filtreleme işlemi, tercih edilen
+//var products = context.Products.Where(product => product.Name == name || product.Name.Contains(name)).ToList();
